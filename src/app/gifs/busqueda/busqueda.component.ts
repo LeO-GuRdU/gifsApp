1d14027f04a7538095a33bb67ac580bac542b3bc
LeO-GuRdU <leogurdulich@gmail.com>
2022-10-13 12:14:44 -0300

    Historial de busqueda creado

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
@@ -6,13 +7,16 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 })
 export class BusquedaComponent {
 
-  constructor() { }
+  constructor(private gifsService : GifsService) { }
 
   @ViewChild('txtBuscar') txtBuscar! : ElementRef<HTMLInputElement>;
 
   buscar(){
     const valor = this.txtBuscar.nativeElement.value;
-    console.log(valor);
+    if(valor.trim().length === 0){
+      return;
+    }
+    this.gifsService.buscarGifs(valor);
     this.txtBuscar.nativeElement.value = '';
   }
 
